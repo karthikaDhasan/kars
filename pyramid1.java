250fcc3584ec5ccc4837d4f47e4899d6901234bb
karthikaDhasan <36623865+karthikaDhasan@users.noreply.github.com>
2018-07-16 15:31:12 +0530

    Update pyramid1.java

--- pyramid1.java
+++ pyramid1.java
@@ -1,39 +1,36 @@
-/* package whatever; // don't place package name! */
+public class pyramid {
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		int n=5,i,k,j,c=0;
-		for(i=1;i<=n;i++)
-		{           
-		
-			for(j=i;j<=n;j++)
-			{
-				System.out.print("\t");
-				
-			}
-			c++;
-			for(k=1;k<=c;k++)
-			{
-		
-			System.out.print(i); 
-		
-			}
-			for(k=1;k<=c;k++)
-			{
-		
-			System.out.print(i); 
-		
-			}
-			c=c+1;
-			System.out.println("\n");
-		            	
-		}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int n=5,m;
+     for(int i=1;i<=n;i++)
+     {
+    	 for(int j=i;j<=n;j++)
+    	 {
+    		 System.out.print(" ");
+    	 }
+    	   
+    		 for( m=1;m<=i;m++)
+    		 {
+    			 System.out.print(m);
+    		 }
+    		 for( m=i-1;m>0;m--)
+    		 {
+    		 System.out.print(m);
+    		 }
+    	 System.out.print("\n");
+    	 
+     }
 	}
+
 }
+
+output:
+        1
+      1 2 1
+    1 2 3 2 1
+  1 2 3 4 3 2 1
+1 2 3 4 5 4 3 2 1
