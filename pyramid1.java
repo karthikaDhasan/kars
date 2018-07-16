c4a098eeda10373547f0a6f54c3c2c63dc2bb59f
karthikaDhasan <36623865+karthikaDhasan@users.noreply.github.com>
2018-07-16 14:41:54 +0530

    Create pyramid1.java

---
+++ pyramid1.java
@@ -0,0 +1,39 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n=5,i,k,j,c=0;
+		for(i=1;i<=n;i++)
+		{           
+		
+			for(j=i;j<=n;j++)
+			{
+				System.out.print("\t");
+				
+			}
+			c++;
+			for(k=1;k<=c;k++)
+			{
+		
+			System.out.print(i); 
+		
+			}
+			for(k=1;k<=c;k++)
+			{
+		
+			System.out.print(i); 
+		
+			}
+			c=c+1;
+			System.out.println("\n");
+		            	
+		}
+	}
+}
