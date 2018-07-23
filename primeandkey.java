d148b07d338cc52715cbe5f33c5b2f83e52dd1e5
karthikaDhasan <36623865+karthikaDhasan@users.noreply.github.com>
2018-07-23 16:23:21 +0530

    Create primeandkey.java

---
+++ primeandkey.java
@@ -0,0 +1,34 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+	// your code goes here
+	int s=0,t=0;
+	for(int i=11;i<=27;i++)
+	{
+		for(int j=2;j<i;j++)
+		{
+			int c=0;
+			if(i%j==0)
+			{
+			 c=1;
+			}
+			if(c==0)
+			{ 
+				t++;
+				b[s]=i;
+				s++
+			}
+		}
+	}
+     
+	}
+}
