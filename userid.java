84d6712e08944b25f764eb0b3b5b1ac28be39227
karthikaDhasan <36623865+karthikaDhasan@users.noreply.github.com>
2018-07-26 16:07:34 +0530

    Create userid.java

---
+++ userid.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+
+public class pgm {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String fn=sc.nextLine();
+		String ln=sc.nextLine();
+		String sn="",bn="";
+		char a1,a2 ;
+        int pn=sc.nextInt();
+        int n=sc.nextInt(),i;
+       /* System.out.println("First name="+" "+fn);
+        System.out.println("Last name="+" "+ln);
+        System.out.println("PIN="+" "+pn);
+        System.out.println("N="+" "+ n);*/
+        if(fn.length()!=ln.length())
+        {
+        if(fn.length()<ln.length())
+        {
+        	sn=fn;
+        	bn=ln;
+        }
+        else if(fn.length()>ln.length())
+        {
+        	bn=fn;
+        	sn=ln;
+        }
+        }//if
+        else
+        {
+        	for(i=0;i<fn.length();i++)
+        	{
+        	int a=fn.charAt(i);
+    		int b=ln.charAt(i);
+    		if(a!=b)
+    		{
+    			if(a>b)
+    			{
+    				//System.out.println(a);
+    				//System.out.println(b);
+    				bn=fn;
+    				sn=ln;
+    			}
+    			else
+    			{
+    				sn=fn;
+    				bn=ln;
+    			}
+    		}//if
+    		break;
+        	}
+        	
+        }//else
+        System.out.println("smaller name:"+sn);
+        System.out.println("larger name:"+bn.charAt(0));
+        
+        String s=String.valueOf(pn);
+        a1=s.charAt(n-1);
+        a2=s.charAt(s.length()-n);
+       System.out.println("Index L-R:"+a1);
+       System.out.println("Index R-L:"+a2);
+       String main=bn.charAt(0)+sn+a1+a2;
+       System.out.println("user-id:"+" " +main);
+       System.out.println("user-id()after toggle:");
+       for(i=0;i<main.length();i++)
+       {
+    	  char c=main.charAt(i) ;
+    	  if(Character.isUpperCase(c))
+    	  {
+    		  System.out.print(Character.toLowerCase(c));
+    	  }
+    	  else
+    	  {
+    		  System.out.print(Character.toUpperCase(c));
+    	  }
+    		  
+       }
+        
+
+	}
+
+}
+
+
+OUTPUT:
+RAJIV
+Roy
+624003
+2
+//smaller name:Roy
+larger name:R
+Index L-R:2
+Index R-L:0
+user-id: RRoy20
+user-id()after toggle:
+rrOY20
