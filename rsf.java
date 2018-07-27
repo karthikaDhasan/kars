36bb482414fa08859f0037c4d6d0315ef562908f
karthikaDhasan <36623865+karthikaDhasan@users.noreply.github.com>
2018-07-27 11:48:51 +0100

    Update rsf.java

--- rsf.java
+++ rsf.java
@@ -1,50 +1,36 @@
-/* package whatever; // don't place package name! */
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+public class jeyarsf {
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		int n=5271,i,c,d;
-		if(n>=100)
-		{
-			String str=" ";
-			String s=Integer.toString(n);
-			for(i=0;i<s.length()-1;i++)
-			{ 
-				
-				int a=Integer.parseInt(Character.toString(s.charAt(i)));
-				int b=Integer.parseInt(Character.toString(s.charAt(i+1)));
-			     c=a-b;
-			    d=Math.abs(c);
-			    str=str+Integer.toString(d);
-		                
-			}
-			System.out.println(str);
-			String  s3=str;
-			int e=Integer.parseInt(s3);
-			String str1="0";
-			if(e>99)
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a="67243";
+		System.out.println("given:"+a);
+		String s=a;
+	
+		            while(s.length()>2)
+		            {
+		            		String c1="";
+			for(int i=0;i<s.length()-1;i++)
 			{
-			 String s1=Integer.toString(e);
-			for(i=0;i<s1.length()-1;i++)
-			{ 
-				
-				int a=Integer.parseInt(Character.toString(s1.charAt(i)));
-				int b=Integer.parseInt(Character.toString(s1.charAt(i+1)));
-			     c=a-b;
-			     d=Math.abs(c);
-			    str1=str1+Integer.toString(d);
-		                
-			}	
+				int x=Integer.parseInt(String.valueOf(s.charAt(i)));
+				int y=Integer.parseInt(String.valueOf(s.charAt(i+1)));
+				 int g=x-y;
+				 int g1=Math.abs(g);
+				 c1=c1+g1;
 			}
-			System.out.println(str1);
 			
-		}
+	                         s=c1;
+	                         System.out.println(s);
+		           }
 	}
+
 }
+output:
+
+given:67243
+1521
+431
+12
